6d277ceed91a29d6dd2435333a6ad6fa18cc1b5c
yousefsul <67277838+yousefsul@users.noreply.github.com>
2021-03-13 18:18:36 +0200

    adding user model for registration and login

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User
+
+=begin
+  class User
+    define user attribute email,password and admin for registration and login
+    admin attribute default as false
+    using the activemodel to secure the user password
+    email and password can't be blank
+=end
+
+  include Mongoid::Document
+  include Mongoid::Attributes::Dynamic
+  include ActiveModel::SecurePassword
+  field :email, type: String
+  field :password_digest, type: String
+  field :admin, type: Boolean, default => false
+
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+end
